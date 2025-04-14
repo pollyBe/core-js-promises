aa103adf50b006830b3748a0226c3c5501731acb
Polina Belash <polinka.zhuravlinka@gmail.com>
2025-04-15 00:57:07 +0300

    fix: fixed linter errors. add solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -81,23 +81,25 @@ function getFirstResolvedPromiseResult(promises) {
 function getFirstPromiseResult(promises) {
   return new Promise((resolve, reject) => {
     if (!promises || promises.length === 0) {
-      reject(new Error("Array of promises is empty or null"));
+      reject(new Error('Array of promises is empty or null'));
       return;
     }
 
-    let rejectionReasons = [];
+    const rejectionReasons = [];
 
-    promises.forEach(promise => {
+    promises.forEach((promise) => {
       Promise.resolve(promise)
-          .then(value => {
-            resolve(value);
-          })
-          .catch(reason => {
-            rejectionReasons.push(reason);
-            if (rejectionReasons.length === promises.length) {
-              reject(new AggregateError(rejectionReasons, "All promises were rejected"));
-            }
-          });
+        .then((value) => {
+          resolve(value);
+        })
+        .catch((reason) => {
+          rejectionReasons.push(reason);
+          if (rejectionReasons.length === promises.length) {
+            reject(
+              new AggregateError(rejectionReasons, 'All promises were rejected')
+            );
+          }
+        });
     });
   });
 }
@@ -130,7 +132,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
 function getAllResult(promises) {
-  return Promise.allSettled(promises);
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return result.value;
+      }
+      return null;
+    });
+  });
 }
 
 /**
